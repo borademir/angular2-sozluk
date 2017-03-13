69c738e4ac3d6b82ece23a498a9b4d99fb90d948
Bora Demir <bora@jforce.com.tr>
2017-03-13 11:28:57 +0300

    initial

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { Angular2CliTestPage } from './app.po';
+
+describe('angular2-cli-test App', () => {
+  let page: Angular2CliTestPage;
+
+  beforeEach(() => {
+    page = new Angular2CliTestPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

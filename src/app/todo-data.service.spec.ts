fecb2e35dc3e433c14f04153916725ac9ad167b4
Bora Demir <bora@jforce.com.tr>
2017-03-13 12:16:18 +0300

    todo service

---
+++ src/app/todo-data.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TodoDataService } from './todo-data.service';
+
+describe('TodoDataService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoDataService]
+    });
+  });
+
+  it('should ...', inject([TodoDataService], (service: TodoDataService) => {
+    expect(service).toBeTruthy();
+  }));
+});
---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+
+@Injectable()
+export class TodoDataService {
+
+  /**
+   * service instance seviyesinde, session scope gibi sanirim , degisken store ediyoruz.
+   */
+  lastId: number = 0;
+
+  todos: Todo[] = [];
+
+  constructor() { }
+
+  addTodo(pTodo: Todo): TodoDataService {
+    if(!pTodo.id){
+      pTodo.id = ++this.lastId;
+    }
+    this.todos.push(pTodo);
+    return this;
+  }
+  
+  /**
+   * 
+   * todo : array filter nasil calisiyor ??
+   */
+  deleteTodoById(pId: number): TodoDataService {
+    this.todos = this.todos.filter(todo => todo.id !== pId);
+    return this;
+  }
+
+  getAllTodos(): Todo[] {
+    return this.todos;
+  }
+
+  getTodoById(pId: number): Todo {
+    return this.todos.filter(todo => todo.id === pId).pop();
+  }
+
+  updateTodoById(pId : number, values: Object = {}): Todo {
+    let targetTodo = this.getTodoById(pId);
+    if(!targetTodo){
+      return null;
+    }
+
+    Object.assign(targetTodo, values);
+    return targetTodo;
+  }
+
+  toggleTodoComplete(todo : Todo): Todo {
+    let targetTodo = this.updateTodoById(todo.id, {
+      compplete : !todo.complete
+    });
+    
+    return targetTodo;
+  }
+}

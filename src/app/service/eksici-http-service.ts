293d3378cb5502af73d47bfbc35209fb132747bf
Bora Demir <bora@jforce.com.tr>
2017-05-11 11:51:59 +0300

    topic pager dropdown

--- src/app/service/eksici-http-service.ts
+++ src/app/service/eksici-http-service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class EksiciService {
-  private apiBaseUrl = 'http://localhost:9080/api/v1/';  // URL to web API
+  private apiBaseUrl = 'http://localhost:8080/v1/';  // URL to web API
   private apiBaseUrl_eksici = 'http://www.ekcisi.com/api/v1/';  // URL to web API
   
   constructor (private http: Http) {}

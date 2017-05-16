fbbb3427ad28253f7627c552f6f90c0643de1058
Bora Demir <bora@jforce.com.tr>
2017-05-16 12:04:55 +0300

    code cleaning

--- src/app/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CollapseDirective } from 'ng2-bootstrap/ng2-bootstrap'
-@NgModule({
-  declarations: [ CollapseDirective ],
-  exports: [ CollapseDirective ]
-})
-export class SharedModule {
-
-}
